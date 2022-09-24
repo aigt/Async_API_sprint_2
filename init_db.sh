b9faa0cd256ca0a3c7a5cdd98bc70b1331a52fdb
Kutaraev <artem.kutaraev@gmail.com>
2022-09-24 18:49:58 +0300

    Замечания по докеру устранены

--- init_db.sh
+++ init_db.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/db_init/movies_database.ddl
+psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/postgres/movies_database.ddl
 
-psql -U postgres -d postgres < /docker-entrypoint-initdb.d/db_init/movies_db_dump.sql
+psql -U postgres -d postgres < /docker-entrypoint-initdb.d/postgres/movies_db_dump.sql
 

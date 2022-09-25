2fdd30f3daed35d8314102f33759a8e240645f70
aigt <ai-kr@yandex.ru>
2022-09-25 12:08:22 +0500

    Добавлено более подроное описание к классу ParamWithOption

--- movies_api/src/models/request/param_with_option.py
+++ movies_api/src/models/request/param_with_option.py
@@ -6,7 +6,14 @@ from pydantic import BaseModel
 class ParamWithOption(BaseModel):
     """Объект параметра с опцией.
 
-    Например, приходящий в строке filter[genres]=horror
+    Например, в адресе запроса:
+    /api/v1/films/?filter[genres]=horror&page[size]=50&sort=-imdb_rating
+
+    будут 3 параметра:
+            1       2       3
+    name:   filter  page    sort
+    option: genres  size    None
+    value:  horror  50      -imdb_rating
     """
 
     name: str

af4c484e26fb17ec7884c56d8d1c1140e0348d98
aigt <ai-kr@yandex.ru>
2022-09-26 21:40:42 +0500

    Рефакторинг настроек postgres

--- init_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/postgres/movies_database.ddl
-
-psql -U postgres -d postgres < /docker-entrypoint-initdb.d/postgres/movies_db_dump.sql
-

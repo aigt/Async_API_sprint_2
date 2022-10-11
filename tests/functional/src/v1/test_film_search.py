05709f9614a54148b4ea9b21dcc8444b134276e3
Kutaraev <artem.kutaraev@gmail.com>
2022-10-11 22:54:27 +0300

    тесты жанров параметризованы

--- tests/functional/src/v1/test_film_search.py
+++ tests/functional/src/v1/test_film_search.py
@@ -7,8 +7,63 @@ from tests.functional.testdata.conftest import get_es_bulk_query, es_client, es_
 from tests.functional.testdata.load_to_es_data import es_films_search, es_films
 
 
+# @pytest.mark.asyncio
+# async def test_films_search(es_write_data, make_get_request):
+#     bulk_query = get_es_bulk_query(data=es_films_search,
+#                                    index=test_settings.es_index['films_search'],
+#                                    id_field=test_settings.es_id_field)
+#
+#     await es_write_data(bulk_query=bulk_query, index=test_settings.es_index['films_search'])
+#
+#     url = test_settings.service_url + '/api/v1/films/search'
+    # # проверка успешного поиска с параметром query
+    # query_data = {'query': 'The Star', 'page[size]': 50}
+    # response = await make_get_request(url=url, query_data=query_data)
+    # body = await response.json()
+    # status = response.status
+    # assert status == 200
+    # assert len(body) == 50
+    #
+    # # проверка поиска неизвестного фильма
+    # query_data_unknown = {'query': 'Unknown Movie', 'page[size]': 50}
+    # response_unknown = await make_get_request(url=url, query_data=query_data_unknown)
+    # body_unknown = await response_unknown.json()
+    # status_unknown = response_unknown.status
+    # assert status_unknown == 404
+    # assert len(body_unknown) == 1
+    #
+    # # проверка ошибки при пустом значении query
+    # response_empty = await make_get_request(url=url, query_data=None)
+    # body_empty = await response_empty.json()
+    # status_empty = response_empty.status
+    # assert status_empty == 422
+    # assert len(body_empty) == 1
+    #
+    # # проверка вывода количества записей на страницу
+    # query_data = {'query': 'The Star', 'page[size]': 1}
+    # response = await make_get_request(url=url, query_data=query_data)
+    # body = await response.json()
+    # status = response.status
+    # assert status == 200
+    # assert len(body) == 1
+
+    # # проверка вывода второй страницы
+    # query_data = {'query': 'The Star', 'page[size]': 1, 'page[number]': 2}
+    # response = await make_get_request(url=url, query_data=query_data)
+    # body = await response.json()
+    # status = response.status
+    # assert status == 200
+    # assert body[0]['title'] == 'The Star 1'
+
+
+
+@pytest.mark.parametrize("query_data, expected",
+                         [({'query': 'The Star', 'page[size]': 50}, {'status': 200, 'length': 50}),
+                          ({'query': 'Unknown Movie', 'page[size]': 50}, {'status': 404, 'length': 1}),
+                          (None, {'status': 422, 'length': 1}),
+                          ({'query': 'The Star', 'page[size]': 1}, {'status': 200, 'length': 1})])
 @pytest.mark.asyncio
-async def test_films_search(es_write_data, make_get_request):
+async def test_films_search(es_write_data, make_get_request, query_data, expected):
     bulk_query = get_es_bulk_query(data=es_films_search,
                                    index=test_settings.es_index['films_search'],
                                    id_field=test_settings.es_id_field)
@@ -16,41 +71,27 @@ async def test_films_search(es_write_data, make_get_request):
     await es_write_data(bulk_query=bulk_query, index=test_settings.es_index['films_search'])
 
     url = test_settings.service_url + '/api/v1/films/search'
-    # проверка успешного поиска с параметром query
-    query_data = {'query': 'The Star', 'page[size]': 50}
     response = await make_get_request(url=url, query_data=query_data)
     body = await response.json()
     status = response.status
-    assert status == 200
-    assert len(body) == 50
+    assert status == expected['status']
+    assert len(body) == expected['length']
 
-    # проверка поиска неизвестного фильма
-    query_data_unknown = {'query': 'Unknown Movie', 'page[size]': 50}
-    response_unknown = await make_get_request(url=url, query_data=query_data_unknown)
-    body_unknown = await response_unknown.json()
-    status_unknown = response_unknown.status
-    assert status_unknown == 404
-    assert len(body_unknown) == 1
 
-    # проверка ошибки при пустом значении query
-    response_empty = await make_get_request(url=url, query_data=None)
-    body_empty = await response_empty.json()
-    status_empty = response_empty.status
-    assert status_empty == 422
-    assert len(body_empty) == 1
+@pytest.mark.parametrize("query_data, expected",
+                         [({'query': 'The Star', 'page[size]': 1, 'page[number]': 2}, {'status': 200, 'title': 'The Star 1'})])
 
-    # проверка вывода количества записей на страницу
-    query_data = {'query': 'The Star', 'page[size]': 1}
+@pytest.mark.asyncio
+async def test_films_search_title(es_write_data, make_get_request, query_data, expected):
+    bulk_query = get_es_bulk_query(data=es_films_search,
+                                   index=test_settings.es_index['films_search'],
+                                   id_field=test_settings.es_id_field)
+
+    await es_write_data(bulk_query=bulk_query, index=test_settings.es_index['films_search'])
+
+    url = test_settings.service_url + '/api/v1/films/search'
     response = await make_get_request(url=url, query_data=query_data)
     body = await response.json()
     status = response.status
-    assert status == 200
-    assert len(body) == 1
-
-    # проверка вывода второй страницы
-    query_data = {'query': 'The Star', 'page[size]': 1, 'page[number]': 2}
-    response = await make_get_request(url=url, query_data=query_data)
-    body = await response.json()
-    status = response.status
-    assert status == 200
-    assert body[0]['title'] == 'The Star 1'
+    assert status == expected['status']
+    assert body[0]['title'] == expected['title']

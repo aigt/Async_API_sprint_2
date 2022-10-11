1a7298c5165caa465fcc750e3db254144068c62c
Kutaraev <artem.kutaraev@gmail.com>
2022-10-11 22:07:42 +0300

    тесты фильмов параметризованы

--- tests/functional/src/v1/test_film.py
+++ tests/functional/src/v1/test_film.py
@@ -7,8 +7,11 @@ from tests.functional.testdata.conftest import get_es_bulk_query, es_client, es_
 from tests.functional.testdata.load_to_es_data import es_films_search, es_films
 
 
+@pytest.mark.parametrize("query_data, expected",
+                         [(None, {"status": 200, "length": 2}),
+                          ({'page[size]': 1}, {"status": 200, "length": 1})])
 @pytest.mark.asyncio
-async def test_films(es_write_data, make_get_request):
+async def test_films_example(es_write_data, make_get_request, query_data, expected):
     bulk_query = get_es_bulk_query(data=es_films,
                                    index=test_settings.es_index['films'],
                                    id_field='id')
@@ -17,64 +20,62 @@ async def test_films(es_write_data, make_get_request):
     url = test_settings.service_url + '/api/v1/films/'
 
     # проверка успешного вывода всех фильмов
+    response = await make_get_request(url=url, query_data=query_data)
+    body = await response.json()
+    status = response.status
+    assert status == expected['status']
+    assert len(body) == expected['length']
+
+
+@pytest.mark.parametrize("query_data, expected",
+                         [({'page[size]': 1, 'page[number]': 2}, {"status": 200, "title": 'The Shining'}),
+                          ({'sort': '-imdb_rating'}, {"status": 200, "title": 'The Shining'}),
+                          ({'filter[genre]': 'horror'}, {"status": 200, "title": 'The Shining'})])
+@pytest.mark.asyncio
+async def test_films_example_2(es_write_data, make_get_request, query_data, expected):
+    bulk_query = get_es_bulk_query(data=es_films,
+                                   index=test_settings.es_index['films'],
+                                   id_field='id')
+
+    await es_write_data(bulk_query=bulk_query, index=test_settings.es_index['films'])
+    url = test_settings.service_url + '/api/v1/films/'
+
+    response = await make_get_request(url=url, query_data=query_data)
+    body = await response.json()
+    status = response.status
+    assert status == expected['status']
+    assert body[0]['title'] == expected['title']
+
+
+@pytest.mark.parametrize("film_id, expected",
+                         [(es_films[0]['id'], {'status': 200, 'title': 'Star Wars'})])
+@pytest.mark.asyncio
+async def test_films_example_3(es_write_data, make_get_request, film_id, expected):
+    bulk_query = get_es_bulk_query(data=es_films,
+                                   index=test_settings.es_index['films'],
+                                   id_field='id')
+
+    await es_write_data(bulk_query=bulk_query, index=test_settings.es_index['films'])
+    url = test_settings.service_url + '/api/v1/films/' + film_id
     response = await make_get_request(url=url, query_data=None)
     body = await response.json()
     status = response.status
-    assert status == 200
-    assert len(body) == 2
+    assert status == expected['status']
+    assert body['title'] == expected['title']
 
-    # проверка ограниничения количества фильмов на страницу
-    query_data = {'page[size]': 1}
-    response = await make_get_request(url=url, query_data=query_data)
+
+@pytest.mark.parametrize("film_id, expected",
+                         [('fa189edd-9f2b-4d21-ac33-895890a93632', {'status': 404}),
+                          ('some invalid id', {'status': 422})])
+@pytest.mark.asyncio
+async def test_films_example_4(es_write_data, make_get_request, film_id, expected):
+    bulk_query = get_es_bulk_query(data=es_films,
+                                   index=test_settings.es_index['films'],
+                                   id_field='id')
+
+    await es_write_data(bulk_query=bulk_query, index=test_settings.es_index['films'])
+    url = test_settings.service_url + '/api/v1/films/' + film_id
+    response = await make_get_request(url=url, query_data=None)
     body = await response.json()
     status = response.status
-    assert status == 200
-    assert len(body) == 1
-
-    # проверка вывода фильмов на второй странице
-    query_data = {'page[size]': 1, 'page[number]': 2}
-    response = await make_get_request(url=url, query_data=query_data)
-    body = await response.json()
-    status = response.status
-    assert status == 200
-    assert body[0]['title'] == 'The Shining'
-
-    # проверка работы сортировки по рейтингу (убывание)
-    query_data = {'sort': '-imdb_rating'}
-    response = await make_get_request(url=url, query_data=query_data)
-    body = await response.json()
-    status = response.status
-    assert status == 200
-    assert body[0]['title'] == 'The Shining'
-
-    # проверка работы фильтрации по жанру
-    query_data = {'filter[genre]': 'horror'}
-    response = await make_get_request(url=url, query_data=query_data)
-    body = await response.json()
-    status = response.status
-    assert status == 200
-    assert body[0]['title'] == 'The Shining'
-
-    # проверка поиска фильма по id
-    url_id = url + es_films[0]['id']
-    response = await make_get_request(url=url_id, query_data=None)
-    body = await response.json()
-    status = response.status
-    assert status == 200
-    assert body['title'] == 'Star Wars'
-
-    # проверка поиска фильма по неизвестному id
-    url_id = url + 'fa189edd-9f2b-4d21-ac33-895890a93632'
-    response = await make_get_request(url=url_id, query_data=None)
-    body = await response.json()
-    status = response.status
-    assert status == 404
-    assert body['detail'] == 'film not found'
-
-    # проверка поиска фильма по невалидному id
-    url_id = url + 'some invalid id'
-    response = await make_get_request(url=url_id, query_data=None)
-    body = await response.json()
-    status = response.status
-    assert status == 422
-
+    assert status == expected['status']

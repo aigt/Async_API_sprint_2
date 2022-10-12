153b87579e42a6b68660a0914df1bd4d19da7a7e
Kutaraev <artem.kutaraev@gmail.com>
2022-10-12 19:18:00 +0300

    wrapper возваращает результат выполнения функции etl (то есть None)

--- etl/src/decorators/period.py
+++ etl/src/decorators/period.py
@@ -6,7 +6,7 @@ def period(secs: int) -> Callable:
     """Декоратор для установки периодичности запуска etl-процесса"""
 
     def decorator(func: Callable) -> Callable:
-        def wrapper(*args, **kwargs) -> Callable:
+        def wrapper(*args, **kwargs) -> None:
             time.sleep(secs)
             return func(*args, **kwargs)
 

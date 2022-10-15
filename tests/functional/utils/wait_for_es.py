df869a8b88a59299dd206141a347eb5b247385cf
aigt <ai-kr@yandex.ru>
2022-10-15 11:50:40 +0500

    Тестам добавлены вэйтеры

---
+++ tests/functional/utils/wait_for_es.py
@@ -0,0 +1,17 @@
+import time
+
+from elasticsearch import Elasticsearch
+
+from settings import get_settings
+
+if __name__ == '__main__':
+    es_client = Elasticsearch(
+        hosts=get_settings().es_host,
+        verify_certs=False
+    )
+    while True:
+        if es_client.ping():
+            print('Elasticsearch connected.')
+            break
+        print('Elasticsearch not connected, retry in 1 seconds...')
+        time.sleep(1) 
